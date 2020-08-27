a05c791e6b3dac88ff6f0da96c81962bc097fd95
Nathan Fish <lordcirth@gmail.com>
2020-08-27 19:34:58 -0400

    try setting allowUnfree in flake.nix

--- flake.nix
+++ flake.nix
@@ -1,8 +1,11 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.03";
 
-  outputs = { self, nixpkgs }: {
-    defaultPackage.x86_64-linux =
+  outputs = { self, nixpkgs }: rec {
+
+    defaultPackage = ( import nixpkgs { config.allowUnfree = true; }).callPackage rawPackage;
+
+    rawPackage.x86_64-linux =
       with import nixpkgs { system = "x86_64-linux"; };
       stdenv.mkDerivation rec {
         name = "aurora-wine";
@@ -13,14 +16,16 @@
           sha256    = "aab11f817be1131cc32efebd2a02faf0bf0600a2af7ef971ff32f290a581ef3d";
         };
 
+        src2 = pkgs.fetchurl {
+          curlOpts  = [ "--user-agent" "Wget" ];
+          url       = "http://pentarch.org/steve/Aurora1110.rar";
+          sha256    = "61777018f5e39a536285e4cb86cc730d8df323539b1bf4c4e68736b786423bde";
+        };
         setup-hooks = [ unrar ];
 
-#        unpackPhase = ''
-#          mkdir src && cd src
-#          unar ${src}
-#        '';
         installPhase = ''
           cp -r src $out
+          cp src2/* $out/
         '';
       };
   };
