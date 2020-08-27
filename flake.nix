ece69b5d50c377dffd5778e02c1579a8a03b0c30
Nathan Fish <lordcirth@gmail.com>
2020-08-27 19:41:05 -0400

    use allowUnfree

--- flake.nix
+++ flake.nix
@@ -3,10 +3,8 @@
 
   outputs = { self, nixpkgs }: rec {
 
-    defaultPackage = ( import nixpkgs { config.allowUnfree = true; }).callPackage rawPackage;
-
-    rawPackage.x86_64-linux =
-      with import nixpkgs { system = "x86_64-linux"; };
+    defaultPackage.x86_64-linux =
+      with import nixpkgs { config.allowUnfree = true; system = "x86_64-linux"; };
       stdenv.mkDerivation rec {
         name = "aurora-wine";
         # version = "1.11"
@@ -16,17 +14,17 @@
           sha256    = "aab11f817be1131cc32efebd2a02faf0bf0600a2af7ef971ff32f290a581ef3d";
         };
 
-        src2 = pkgs.fetchurl {
-          curlOpts  = [ "--user-agent" "Wget" ];
-          url       = "http://pentarch.org/steve/Aurora1110.rar";
-          sha256    = "61777018f5e39a536285e4cb86cc730d8df323539b1bf4c4e68736b786423bde";
-        };
+#        src2 = pkgs.fetchurl {
+#          curlOpts  = [ "--user-agent" "Wget" ];
+#          url       = "http://pentarch.org/steve/Aurora1110.rar";
+#          sha256    = "61777018f5e39a536285e4cb86cc730d8df323539b1bf4c4e68736b786423bde";
+#        };
         setup-hooks = [ unrar ];
 
-        installPhase = ''
-          cp -r src $out
-          cp src2/* $out/
-        '';
+#        installPhase = ''
+#          cp -r src $out
+#          cp src2/* $out/
+#        '';
       };
   };
 }
