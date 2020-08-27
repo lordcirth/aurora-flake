36e4db0651e4a68fc2d7a0f2f1a823fb4f67a523
Nathan Fish <lordcirth@gmail.com>
2020-08-27 19:09:29 -0400

    fix curl and try unrar

--- flake.nix
+++ flake.nix
@@ -4,13 +4,24 @@
   outputs = { self, nixpkgs }: {
     defaultPackage.x86_64-linux =
       with import nixpkgs { system = "x86_64-linux"; };
-      stdenv.mkDerivation {
+      stdenv.mkDerivation rec {
         name = "aurora-wine";
         # version = "1.11"
         src = pkgs.fetchurl {
-          url     = "http://pentarch.org/steve/Aurora151Full.rar";
-          sha256  = "0000000000000000000000000000000000000000000000000000";
+          curlOpts  = [ "--user-agent" "Wget" ];
+          url       = "http://pentarch.org/steve/Aurora151Full.rar";
+          sha256    = "aab11f817be1131cc32efebd2a02faf0bf0600a2af7ef971ff32f290a581ef3d";
         };
+
+        setup-hooks = [ unrar ];
+
+#        unpackPhase = ''
+#          mkdir src && cd src
+#          unar ${src}
+#        '';
+        installPhase = ''
+          cp -r src $out
+        '';
       };
   };
 }
